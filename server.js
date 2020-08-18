d01624b2f5e030081685c40ea2d4e2d01d83f37b
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-08-18 19:41:10 +0530

    build graphql server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema.js')
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+	schema,
+	graphiql: true
+}));
+
+app.listen(4000, ()=>{console.log('server started on 4000')})
